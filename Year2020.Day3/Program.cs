64cd2f5302706e56fe3ffd2320af446b84ec4a0b
lohithbb <lohithbb@hotmail.com>
2020-12-07 05:17:32 +0000

    added Year2020.Day3 - Part 1

--- Year2020.Day3/Program.cs
+++ Year2020.Day3/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
 
 namespace Year2020.Day3
 {
@@ -6,7 +9,75 @@ namespace Year2020.Day3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Advent of Code - Year 2020 - Day 2");
+
+            // input.txt should be present in the /bin directory (Properties > Copu to output directory = Copy always)
+            var input = File.ReadAllLines("input.txt");
+
+            // prepare input data       
+            // convert string[] -> char[][] (jagged)
+            char[][] charInput = ConvertString1DArrayToJaggedChar2DArray(input);
+
+            //PrintChar2DArray(charInput);
+
+
+            #region Part 1
+            Console.WriteLine("Part 1");
+
+            int numberOfTrees = 0;
+
+            // start at [0,0] -> [i,j]
+            // move [1,3] -> 1 down, 3 across
+            int x = 0;
+            int y = 0;
+
+            int deltaX = 1;
+            int deltaY = 3;
+
+            while (x + deltaX < charInput.Length)
+            {
+                x = x + deltaX;
+                y = (y + deltaY) % charInput[x].Length;
+
+                if (charInput[x][y] == '.')
+                {
+                    charInput[x][y] = 'O';
+                    continue;
+                }
+                if (charInput[x][y] == '#')
+                {
+                    charInput[x][y] = 'X';
+                    numberOfTrees++;
+                    continue;
+                }
+            }
+
+            //PrintChar2DArray(charInput);
+            Console.WriteLine($"Number of trees encountered : {numberOfTrees}");
+
+            #endregion
+
         }
+
+        static char[][] ConvertString1DArrayToJaggedChar2DArray(string[] stringArray)
+        {
+            char[][] charArray = new char[stringArray.Length][];
+
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                charArray[i] = stringArray[i].ToArray();
+            }
+
+            return charArray;
+        }
+
+        static void PrintChar2DArray(char[][] char2DArray)
+        {
+            for (int i = 0; i < char2DArray.Length; i++)
+            {
+                Console.WriteLine(new string(char2DArray[i]));
+            }
+        }
+
     }
 }
