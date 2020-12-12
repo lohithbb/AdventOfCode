3700f3b0c005cb85698d581bde598360bedd45d4
lohithbb <lohithbb@hotmail.com>
2020-12-12 05:39:03 +0000

    completed Year2020.Day6 - Part 2

--- Year2020.Day6/Program.cs
+++ Year2020.Day6/Program.cs
@@ -31,20 +31,47 @@ namespace Year2020.Day6
                 .Select(c => String.Concat(c.ToCharArray().OrderBy(c => c).Distinct()));
 
             // a count is essentially the sum of distinct characters in a group
-            var sumOfCounts = sanitisedInput
+            var sumOfCountsPart1 = sanitisedInput
                 .Select(l => l.Count())
                 .Sum();
 
             // print output
-            foreach (var line in sanitisedInput)
-            {
-                Console.WriteLine(line);
-            }
-            Console.WriteLine($"Sum of counts : {sumOfCounts}");
-
+            //foreach (var line in sanitisedInput)
+            //{
+            //    Console.WriteLine(line);
+            //}
+            Console.WriteLine($"Sum of counts : {sumOfCountsPart1}");
 
             #endregion
 
+
+            #region Part 2
+
+            Console.WriteLine("Part 2");
+
+            // need to slightly change the way the input gets sanitised
+            sanitisedInput = groupedInput
+                // replace newlines with a space
+                .Select(g => g.Replace(Environment.NewLine, " "));
+
+            var sumOfCountsPart2 = sanitisedInput
+                // split each group into each persons
+                .Select(g => g.Split(" "))
+
+                // not a bad code relic - this shows the inner workings of how i developed the aggregate function
+                //.Select(g => g.Aggregate(
+                //    "qwertyuiopasdfghjklzxcvbnm", // faceroll on the keyboard
+                //    (result, next) => new String(result.Intersect(next).ToArray())
+                //    ));
+
+                // Intersect returns a IEnumerable<char> - so we need to convert that back to a string
+                .Select(g => g.Aggregate((a, b) => new string(a.Intersect(b).ToArray())))
+                .Select(g => g.Count())
+                .Sum();
+
+            Console.WriteLine($"Sum of counts : {sumOfCountsPart2}");
+
+            #endregion
         }
     }
 }
